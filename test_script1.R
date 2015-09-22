0d9b852f473c48cbbc2aef2267096ed5cf6cc8dc
Naman Paul <naman.paul@gmail.com>
2015-09-21 23:33:43 -0700

    add class meeting 4 files

---
+++ test_script1.R
@@ -0,0 +1,22 @@
+a <- 2
+b <- 3
+(c <- a+b)
+
+(x <- 1:4)
+
+y <- 32^2
+
+(an_array <- 1:10)
+
+#using cars data
+# Define the cars vector with 5 values
+cars <- c(1, 3, 6, 4, 9)
+
+# Graph cars using blue points overlayed by a line 
+plot(cars, type="o", col="blue")
+
+# Create a title with a red, bold/italic font
+title(main="Autos", col.main="red", font.main=4)
+
+#histogram
+hist(cars)
